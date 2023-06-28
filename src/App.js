7ca769f6450873f60883f2976df8f5ed3f077c58
Sebastian Garcia <121516129+SebCodes27@users.noreply.github.com>
2023-06-28 10:08:23 -0700

    Set changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './index.css';
 import './App.css';
 import React from 'react';
-/* import ReactDOM from 'react-dom/client';*/ 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
